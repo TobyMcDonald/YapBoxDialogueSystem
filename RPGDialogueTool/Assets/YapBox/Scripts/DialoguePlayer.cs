ed1f5ad33d23ba5038b63862695a7e6d2f11879d
EmilyAIE <toby.mcdonald@hotmail.com>
2024-11-23 16:01:46 +1030

    Finished Functionality and created prefab
    The dialogue system is now working how it should!.
    Made a prefab of the dialoguebox maker.
    TO DO. Comment code

--- RPGDialogueTool/Assets/YapBox/Scripts/DialoguePlayer.cs
+++ RPGDialogueTool/Assets/YapBox/Scripts/DialoguePlayer.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.TextCore.Text;
+using System;
 
 
 namespace YapBox
@@ -24,15 +25,15 @@ namespace YapBox
         private List<YapBoxConversation> m_conversationCollection;
         public List<YapBoxConversation> conversationCollection => m_conversationCollection;
 
-        //[SerializeField] private string[] m_text;
-
-        [SerializeField] private int m_dialogueIndex;
-
         [SerializeField] private float m_charDelayTime;
 
         [SerializeField] private GameObject m_dialogueBox;
 
         private DialogueBoxReferences m_dialogueBoxReferences;
+
+        private bool m_isPlaying;
+
+        private Coroutine m_playingText;
 
         // Start is called before the first frame update
         void Start()
@@ -49,50 +50,114 @@ namespace YapBox
         // Update is called once per frame
         void Update()
         {
+            // Calling through SpcaeBar here for testing
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                CreateDialogueBox();
-                m_dialogueIndex++;
+                PlayConversation(0);
             }
         }
 
+
+        /// <summary>
+        /// Play a conversation from the list stored in the DialoguePlayer ConversationList by their index in the list
+        /// </summary>
+        /// <param name="conversationIndex"></param>
         public void PlayConversation(int conversationIndex)
         {
-
+            CreateDialogueBox(conversationIndex);
         }
 
-        private void CreateDialogueBox()
+        private void CreateDialogueBox(int conversationIndex)
         {
             DialogueBoxReferences dbCheck = FindObjectOfType<DialogueBoxReferences>();
             if (!dbCheck)
             {
-                GameObject m_db = Instantiate(m_dialogueBox);
-                StartCoroutine(PlayText(0, m_db.GetComponent<DialogueBoxReferences>(), m_dialogueIndex));
-                LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)m_db.transform);
-                if (!m_showCharacter)
-                    m_db.GetComponent<DialogueBoxReferences>().characterProfile.enabled = false;
+                if (!m_isPlaying)
+                {
+                    m_isPlaying = true;
+                    GameObject m_db = Instantiate(m_dialogueBox);
+                    m_playingText = StartCoroutine(PlayText(conversationIndex, m_db.GetComponent<DialogueBoxReferences>()));
+                    LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)m_db.transform);
+                    if (!m_showCharacter)
+                        m_db.GetComponent<DialogueBoxReferences>().characterProfile.enabled = false;
+                }
+                else 
+                {
+                    if (m_playingText != null)
+                    {
+                        GameObject m_db = Instantiate(m_dialogueBox);
+                        StopCoroutine(m_playingText);
+                        InstantFinishText(conversationIndex, m_db.GetComponent<DialogueBoxReferences>());
+                    }
+
+                }
             }
             else
             {
-                StartCoroutine(PlayText(0, dbCheck, m_dialogueIndex));
-                if (!m_showCharacter)
-                    dbCheck.characterProfile.enabled = false;
+                if (!m_isPlaying)
+                {
+                    if (m_conversationCollection[conversationIndex].conversationPosition > m_conversationCollection[conversationIndex].dialogue.Count - 1)
+                    {
+                        Destroy(dbCheck.gameObject);                 
+                        m_playingText = null;
+                        m_conversationCollection[conversationIndex].conversationPosition = m_conversationCollection[conversationIndex].dialogue.Count - 1;
+                        return;
+                    }
+
+                    m_isPlaying = true;
+                    m_playingText = StartCoroutine(PlayText(conversationIndex, dbCheck));
+                    if (!m_showCharacter)
+                        dbCheck.characterProfile.enabled = false;
+                }
+                else
+                {
+                    if (m_playingText != null)
+                    {
+                        StopCoroutine(m_playingText);
+                        InstantFinishText(conversationIndex, dbCheck);
+                    }
+                }
             }
         }
 
-        private IEnumerator PlayText(int index, DialogueBoxReferences db, int dialogueIndex)
+        private void InstantFinishText(int index, DialogueBoxReferences db)
         {
             db.dialogueText.text = "";
-            foreach (char chara in m_conversationCollection[index].dialogue[dialogueIndex].text)
+            int conversationPos = m_conversationCollection[index].conversationPosition;
+            db.nameText.text = m_conversationCollection[index].dialogue[conversationPos].charName;
+            db.dialogueText.text += m_conversationCollection[index].dialogue[conversationPos].text;
+            db.characterProfile.sprite = m_conversationCollection[index].dialogue[conversationPos].character;
+            db.boxBGSprite.sprite = m_conversationCollection[index].dialogue[conversationPos].bg;
+            db.nameBGSprite.sprite = m_conversationCollection[index].dialogue[conversationPos].characterNameBG;
+            m_conversationCollection[index].conversationPosition++;
+            m_isPlaying = false;
+        }
+
+        private IEnumerator PlayText(int index, DialogueBoxReferences db)
+        {
+            db.dialogueText.text = "";
+            int conversationPos = m_conversationCollection[index].conversationPosition;
+
+            foreach (char chara in m_conversationCollection[index].dialogue[conversationPos].text)
             {
-                db.nameText.text = m_conversationCollection[index].dialogue[dialogueIndex].charName;
+                db.nameText.text = m_conversationCollection[index].dialogue[conversationPos].charName;
                 db.dialogueText.text += chara;
-                db.characterProfile.sprite = m_conversationCollection[index].dialogue[dialogueIndex].character;
-                db.boxBGSprite.sprite = m_conversationCollection[index].dialogue[dialogueIndex].bg;
-                db.nameBGSprite.sprite = m_conversationCollection[index].dialogue[dialogueIndex].characterNameBG;
+                db.characterProfile.sprite = m_conversationCollection[index].dialogue[conversationPos].character;
+                db.boxBGSprite.sprite = m_conversationCollection[index].dialogue[conversationPos].bg;
+                db.nameBGSprite.sprite = m_conversationCollection[index].dialogue[conversationPos].characterNameBG;
                 yield return new WaitForSeconds(m_charDelayTime);
             }
             yield return null;
+            m_conversationCollection[index].conversationPosition++;
+            m_isPlaying = false;
+        }
+
+        private void OnDisable()
+        {
+            foreach (var item in m_conversationCollection)
+            {
+                item.conversationPosition = 0;
+            }
         }
     }
 }
